ed7490a4dddb92d9755bb26c839f884ea65e1830
Your Name <rajatpandey542@gmail.com>
2026-08-31 01:40:12 +0530

    connect API routes with database

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,13 +1,19 @@
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/prisma";
 
-export function GET(): NextResponse {
+export async function GET(): Promise<NextResponse> {
   try {
-    const users = prisma.users.findMany();
-    return NextResponse.json({
-      message: "Devnotes APi is working",
-      users: users,
-    });
+    const users = await prisma.users.findMany();
+
+    return NextResponse.json(
+      {
+        message: "Devnotes APi is workings",
+        users: users,
+      },
+      {
+        status: 201,
+      },
+    );
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
@@ -17,12 +23,25 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
     const body = await request.json();
 
-    return NextResponse.json({
-      message: "Creating Data",
-      body: body,
+    const user = await prisma.users.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password_hash: body.password,
+      },
     });
+
+    return NextResponse.json(
+      {
+        message: "Creating Data",
+        user: user,
+      },
+      {
+        status: 201,
+      },
+    );
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return NextResponse.json(
       {
         message: "There's an error",
