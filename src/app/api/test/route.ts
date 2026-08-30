68c5e0499637b178b7231a6a8139af44d0d0e176
Your Name <rajatpandey542@gmail.com>
2026-08-31 00:16:35 +0530

    connect api to prsima

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,25 +1,36 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
-export async function GET():Promise<NextResponse>{
-    return NextResponse.json({
-        message:"Devnotes APi is working"
-    })
+export async function GET(): Promise<NextResponse> {
+  try {
+    const users = await prisma.users.findMany();
+    return NextResponse.json(
+        {
+      message: "Devnotes APi is working",
+      users : users,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: error },
+      { status: 500 },
+    );
+  }
 }
 
-export async function POST():Promise<NextResponse>{
-    return NextResponse.json({
-        message:"Creating Data",
-    })
+export async function POST(): Promise<NextResponse> {
+  return NextResponse.json({
+    message: "Creating Data",
+  });
 }
 
-export async function PUT():Promise<NextResponse>{
-    return NextResponse.json({
-        message:"Editing Data",
-    })
+export async function PUT(): Promise<NextResponse> {
+  return NextResponse.json({
+    message: "Editing Data",
+  });
 }
 
-export async function DELETE():Promise<NextResponse>{
-    return NextResponse.json({
-        message:"Deleting Data",
-    })
+export async function DELETE(): Promise<NextResponse> {
+  return NextResponse.json({
+    message: "Deleting Data",
+  });
 }
