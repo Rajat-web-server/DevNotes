744a1f475f1afbeafdee7b3d3fdb83a14db6abd5
Your Name <rajatpandey542@gmail.com>
2026-09-03 16:50:25 +0530

    Add routing for auth and signup

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse, NextRequest } from "next/server";
+import { hash } from "bcryptjs";
+import prisma from "@/lib/prisma";
+
+export async  function POST(request:NextRequest){
+    try{
+        const body = await request.json();
+
+        const [name, email, password]= body;
+
+        const existUser = await prisma.users.findUnique({
+            where:{
+                email: email,
+            }
+        });
+        if (existUser){
+            return NextResponse.json({
+                message: "User already exist"
+            },{
+                status:401
+            })
+        }
+
+        const hashedPassword = await hash(password,12);
+
+        const user = await prisma.users.create({
+            data:{
+                name:name,
+                email:email,
+                password_hash:hashedPassword
+            }
+        })
+
+        return NextResponse.json({
+            message: "The signup has been done",
+            data:{
+                name:user.name,
+                email:user.email
+            }
+            
+        })
+
+    }
+    catch(error){
+        console.log(error);
+        return NextResponse.json({
+            message: "There is an error",
+            error
+        },{
+            status:500
+        })
+    }
+}
