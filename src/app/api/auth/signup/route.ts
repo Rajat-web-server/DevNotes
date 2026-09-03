fc85d0a21dcdf41c436bdf143c8f0165769403c3
Your Name <rajatpandey542@gmail.com>
2026-09-03 20:21:43 +0530

    Test the api for GET and POST method

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,69 +2,71 @@ import { NextResponse, NextRequest } from "next/server";
 import { hash } from "bcryptjs";
 import prisma from "@/lib/prisma";
 
-export  function GET():NextResponse{
-    try{
-        
-        return NextResponse.json({
-            message:"This is the signup page"
-        })
-    }
-    catch(error){
-        console.log(error);
-        return NextResponse.json({
-            message:"There's an error",
-            error
-        })
-    }
-
+export function GET(): NextResponse {
+  try {
+    return NextResponse.json({
+      message: "This is the signup page",
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      message: "There's an error",
+      error,
+    });
+  }
 }
 
-export async  function POST(request:NextRequest){
-    try{
-        const body = await request.json();
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
 
-        const [name, email, password]= body;
+    const { name, email, password } = body;
 
-        const existUser = await prisma.users.findUnique({
-            where:{
-                email: email,
-            }
-        });
-        if (existUser){
-            return NextResponse.json({
-                message: "User already exist"
-            },{
-                status:401
-            })
-        }
+    console.log(body);
 
-        const hashedPassword = await hash(password,12);
-
-        const user = await prisma.users.create({
-            data:{
-                name:name,
-                email:email,
-                password_hash:hashedPassword
-            }
-        })
-
-        return NextResponse.json({
-            message: "The signup has been done",
-            data:{
-                name:user.name,
-                email:user.email
-            }
-            
-        })
-
+    const existUser = await prisma.users.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (existUser) {
+      return NextResponse.json(
+        {
+          message: "User already exist",
+        },
+        {
+          status: 409,
+        },
+      );
     }
-    catch(error){
-        console.log(error);
-        return NextResponse.json({
-            message: "There is an error",
-            error
-        },{
-            status:500
-        })
-    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await prisma.users.create({
+      data: {
+        name: name,
+        email: email,
+        password_hash: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({
+      message: "The signup has been done",
+      data: {
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "There is an error",
+        error,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 }
